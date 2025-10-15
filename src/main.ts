a9a1f0799b0d6cf0f099df7d35739496d096bc8d
JasonCho1112001 <jcho111@ucsc.edu>
2025-10-15 22:11:04 +0000

    Step 2: Created a variable redBallClicks and a text to show the value

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,18 @@ const myButton = document.createElement("button");
 document.body.appendChild(myButton);
 
 myButton.textContent = "🔴";
+
+//Step 2
+
+let redBallClicks: number = 0;
+
+//Text element to show number of clicks
+const myText = document.createElement("p");
+myText.textContent = `Red balls: ${redBallClicks}`;
+document.body.appendChild(myText);
+
+//Button click event
+myButton.onclick = () => {
+  redBallClicks++;
+  myText.textContent = `Red balls: ${redBallClicks}`;
+};
